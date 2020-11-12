eee23c60a2f8f561edc02b527b8944e425a0bb10
irisulaj <sulaj.shaqir1@gmail.com>
2020-11-12 01:31:10 +0100

    initial commit completed MyNavbar component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from "react";
+
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar/>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+import Nav from 'react-bootstrap/Nav';
+
+import Logo from "../../assets/icons/icon5.png";
+
+import './mynavbar.style.css';
+
+const MyNavbar = () => {
+
+    return (
+
+        <div>
+      {/* taget html boshe i tregojne reactit qe po e mbeshtjellim kodin ne nje fragment*/}
+    
+<Navbar fixed="top" expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between">
+  <div>
+  <Navbar.Brand href="#hello">
+
+      <img className="logo" src= {Logo}  alt='Ssulaj s Portofolio '/>
+  </Navbar.Brand>
+  </div>
+  <div>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="#hello">Hello</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#skills">Skills</Nav.Link>
+      <Nav.Link href="#education">Education</Nav.Link>
+      <Nav.Link href="#work">My work</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </div>
+</Navbar>
+</div>
+    );
+};
+
+export  default MyNavbar;
